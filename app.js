1f96a735097affd0ea94f4b3b3d5c62595c97a21
Daniel Garcia <dangarpa@gmail.com>
2023-06-09 19:40:40 -0500

    cors enabled

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const { userRoutes } = require("./routes/userRouter");
 const { productRoutes } = require("./routes/productRouter");
 const { carRoutes } = require("./routes/carRouter");
 const { orderRoutes } = require("./routes/orderRouter");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
